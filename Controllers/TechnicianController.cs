99bfccff537b3dc923c9359608ce3dfa691ad47e
KahasimB <kahasim.brown@outlook.com>
2022-03-01 12:02:16 -0600

    Updated sorting in controller w/ ticket status

--- Controllers/TechnicianController.cs
+++ Controllers/TechnicianController.cs
@@ -8,18 +8,27 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using System;
+using JtsAutoShopWeb.Database;
+using JtsAutoShopWeb.Models;
+using System.Linq;
 
 namespace Practice1.Controllers
 {
     public class TechnicianController : Controller
     {
-        //private readonly DatabaseContent _context;
         private readonly ILogger<TechnicianController> _logger;
+        private readonly DatabaseContent _context;
 
         public TechnicianController(ILogger<TechnicianController> logger)
         {
             _logger = logger;
             //_context = context;
+        }
+
+        public TechnicianController(DatabaseContent context)
+        {
+            
+            _context = context;
         }
 
         public IActionResult Technician()
@@ -29,8 +38,54 @@ namespace Practice1.Controllers
             return View();
         }
 
-      
-       
+        public IActionResult Technician(string filterby, bool? des)
+        {
+            List<TicketModel> Results = new List<TicketModel>();
+            switch (filterby)
+            {
+                case "none":
+                    Results = _context.Tickets.ToList();
+                    break;
+                case "prodname":
+                    if (des == true)
+                    {
+                        Results = _context.Tickets.OrderByDescending(x => x.Car).ToList();
+                        break;
+                    }
+                    else
+                    {
+                        Results = _context.Tickets.OrderBy(x => x.Car).ToList();
+                        break;
+                    }
+                case "prodprice":
+                    if (des == true)
+                    {
+                        Results = _context.Tickets.OrderByDescending(x => x.Reservation).ToList();
+                        break;
+                    }
+                    else
+                    {
+                        Results = _context.Tickets.OrderBy(x => x.Reservation).ToList();
+                        break;
+                    }
+                case "prodtype":
+                    if (des == true)
+                    {
+                        Results = _context.Tickets.OrderByDescending(x => x.TicketStatus).ToList();
+                        break;
+                    }
+                    else
+                    {
+                        Results = _context.Tickets.OrderBy(x => x.TicketStatus).ToList();
+                        break;
+                    }
+                default:
+                    Results = _context.Tickets.ToList();
+                    break;
+            }
+            return View(Results);
+        }
+
 
     }
 }
